e925534a55a15cafccf0c890d3bde6efa406b940
hepona <hepona10@gmail.com>
2023-04-23 08:18:21 -0700

    task 0 test

--- _printf.c
+++ _printf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	for (i = 0; format[i] != '\0' ; i++)
 	{
-		if (format[i] != '%' && format[i] != NULL)
+		if (format[i] != '%' || format[i] != NULL)
 		{
 			_putchar(format[i]);
 		}
