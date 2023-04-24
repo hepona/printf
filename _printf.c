51a7760592631934575d37baa617bf774bd5b1c0
hepona <hepona10@gmail.com>
2023-04-23 21:03:08 -0700

    test t0

--- _printf.c
+++ _printf.c
@@ -10,6 +10,7 @@ int _printf(const char *format, ...)
 {
 	va_list list;
 	int i;
+	int count;
 
 	va_start(list, format);
 	if (format == NULL)
@@ -17,29 +18,36 @@ int _printf(const char *format, ...)
 	for (i = 0; format[i] != '\0' ; i++)
 	{
 		if (format[i] != '%')
+		{	
 			_putchar(format[i]);
+			count++;
+		}
 		else
 		{
 			switch (format[i + 1])
 			{
 				case '%':
 					_putchar('%');
+					count++;
 					i++;
 					break;
 				case 'c':
 					print_chara(list);
+					count++;
 					i++;
 					break;
 				case 's':
 					print_string(list);
+					count++;
 					i++;
 					break;
 				default:
 					_putchar(format[i]);
+					count++;
 					break;
 			}
 		}
 	}
 	va_end(list);
-	return (i);
+	return (count);
 }
