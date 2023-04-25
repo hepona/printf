64f3af4f7597fd90445bb2117abf20089c17c1dc
MoroccanMan <a.makhtoubi@gmail.com>
2023-04-25 16:34:21 -0700

    C - printf

--- print_alpha.c
+++ print_alpha.c
@@ -2,21 +2,21 @@
 
 /**
  * print_string - loops through a string and prints
- * every character until end of string character 
+ * every character until end of string character
  * @l: va_list arguments from _printf
  * @f: pointer to the struct flags that determines
  * if a flag is passed to _printf
  * Return: number of char printed
- */
+ *  /
 int print_string(va_list l, flags_t *f)
 {
-    char *s = va_arg(l, char *);
+	char *s = va_arg(l, char *);
 
-    (void)f;
+	(void)f;
 
-    if (!s)
-        s = "(null)";
-    return (_puts(s));
+	if (!s)
+	s = "(null)";
+	return (_puts(s));
 }
 
 /**
@@ -25,10 +25,9 @@ int print_string(va_list l, flags_t *f)
  * @f: pointer to the struct flags that determines
  * if a flag is passed to _printf
  * Return: number of char printed
- */
+ * /
 int print_char(va_list l, flags_t *f)
 {
-    (void)f;
-    _putchar(va_arg(l, int));
-    return (1);
-}
+	(void)f;
+	_putchar(va_arg(l, int));
+	return (1);}
