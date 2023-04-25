c417b0a37ac0dc4b36353683c4cffb4ee7ef8c34
MoroccanMan <a.makhtoubi@gmail.com>
2023-04-25 14:28:17 -0700

    C - printf

--- BACKUP/main.h
+++
@@ -1,9 +0,0 @@
-#include <stddef.h>
-#include <stdarg.h>
-#ifndef MAIN_H
-#define MAIN_H
-int _printf(const char *format, ...);
-int _putchar(char c);
-void print_string(va_list list);
-void print_chara(va_list list);
-#endif
